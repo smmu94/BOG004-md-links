8caf80f6e7f1ce569ab8a462f78bdb72db8458a4
smmu94 <stephaniemmu@gmail.com>
2022-04-26 18:15:45 -0500

    the verifyFile function is deleted and the getAllFiles function is added

--- src/auxFunctions.js
+++ src/auxFunctions.js
@@ -1,51 +1,79 @@
 const fs = require("fs");
 const path = require("path");
+const getLinks = require("./getLinks");
 
-// 1) Verificar si el route es absoluto o relativo, hacerlo absoluto si es relativo
+// Verificar que la ruta ingresada es válida
+
+const verifyRoute = (route) => {
+  if (fs.existsSync(route)) return true;
+};
+
+// Verificar si el route es absoluto o relativo, hacerlo absoluto si es relativo
 
 const pathAbsolute = (route) => {
-    if (path.isAbsolute(route)) return (route = route);
-    else return (route = path.resolve(route));
-  };
-  
-  // const rutaAbsoluta = pathAbsolute('EJEMPLOS MD/src/readme1/Readme1.md');
-  // console.log('ruta absoluta', rutaAbsoluta);
-  
-  // 2) Verificar que el route sea un archivo
-  
-  const verifyFile = (route) => {
-    return new Promise((resolve, reject) => {
-      fs.stat(route, (err, stats) => {
-        if (err) reject(err);
-        resolve(stats.isFile());
-      });
-    });
-  };
-  
-  // verifyFile(rutaAbsoluta);
-  
-  // 3) Extensión de un archivo .md
-  const getExt = (route) => path.extname(route) === ".md";
-  // console.log('el archivo es extensión md?', getExt(rutaAbsoluta));
-  
-  // 4) Leer archivos md
-  const readOneFile = (route) => {
-    return new Promise((resolve, reject) => {
-      fs.readFile(route, "utf-8", (err, data) => {
-        if (err) reject(err);
-        resolve(data);
-      });
-    });
-  };
-  
-  // 5) guardar listado los archivos de un directorio
-  const listAllFiles = (route) => {
-    return fs.readdirSync(route, "utf8");
-  };
+  if (path.isAbsolute(route)) return (route = route);
+  else return (route = path.resolve(route));
+};
 
- module.exports = {pathAbsolute, verifyFile, getExt, readOneFile, listAllFiles};
-  
-  // readAllFiles('/mnt/c/Users/User/proyectosLaboratoria/BOG004-md-links/EJEMPLOS MD');
-  // console.table(readAllFiles('/mnt/c/Users/User/proyectosLaboratoria/BOG004-md-links/ejemplos'));
-  
- 
+// Extensión de un archivo .md
+const getExt = (route) => path.extname(route) === ".md";
+
+// Guardar listado los archivos de un directorio
+const listAllFiles = (route) => {
+  return fs.readdirSync(route, "utf8");
+};
+
+// Funcion para obtener archivos del directorio
+const getAllFiles = (route, files) => {
+  let allFiles = listAllFiles(route);
+  allFiles.forEach((file) => {
+    let absRoute = path.join(route, file);
+    if (getExt(absRoute)) {
+      files.push(absRoute);
+    } else if (fs.statSync(absRoute).isDirectory()) {
+      getAllFiles(absRoute, files);
+    } else {
+      console.log(`El archivo ${absRoute} no es Markdown`);
+    }
+  });
+  return files;
+};
+
+// Leer archivos md
+const readOneFile = (route) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(route, "utf-8", (err, data) => {
+      if (err) reject(err);
+      resolve(getLinks(data, route));
+    });
+  });
+};
+
+module.exports = {
+  verifyRoute,
+  pathAbsolute,
+  getExt,
+  readOneFile,
+  listAllFiles,
+  getAllFiles,
+};
+
+// Verificar que el route sea un archivo
+
+// const verifyFile = (route) => {
+//   fs.stat(route, (err, stats) => {
+//     if (err) throw err;
+//     if (stats.isFile()) {
+//       if (getExt(route)) {
+//         readOneFile(route).then((links) => {return console.log(links)});
+//       } else {
+//         console.log("El archivo no es Markdown");
+//       }
+//     } else {
+//       getAllFiles(route);
+//       files.forEach((file) => {
+//         readOneFile(file).then((links) => {return console.log(links)});
+//       });
+//     }
+//   });
+// };
