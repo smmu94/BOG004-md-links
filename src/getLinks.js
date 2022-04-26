a8d6e0ec65694f3d2a1717d1e01299cc3a167457
smmu94 <stephaniemmu@gmail.com>
2022-04-26 18:13:44 -0500

    modifications are made to return only the links that contain http

--- src/getLinks.js
+++ src/getLinks.js
@@ -2,20 +2,31 @@ const { marked } = require("marked");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-// 7) Funcion para obtener los links de un archivo .md
-
-let html = '';
+const onelink = {
+  href: "",
+  text: "",
+  file: "",
+};
+// Funcion para obtener los links de un archivo .md
 let arrayLinks = [];
-const getLinks = (data) => {
-  html += marked(data);
-  console.log(html)
+const getLinks = (data, file) => {
+  let html = marked(data);
   const dom = new JSDOM(html);
   const document = dom.window.document;
   const links = document.querySelectorAll("a");
+  if (links.length === 0) return console.log("No hay links en el archivo " + file);
+  else {
   links.forEach((link) => {
-    arrayLinks.push(link.href);
+    if(link.href.includes("http")){
+    let newLink = Object.create(onelink);
+    newLink.href = link.href;
+    newLink.text = link.text;
+    newLink.file = file;
+    arrayLinks.push(newLink);
+    }
   });
-  console.log(arrayLinks);
+  return console.log(arrayLinks);
+}
 };
 
 module.exports = getLinks;
